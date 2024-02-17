20fc0b82e39f1c5944251a6a764a57869e8d76ab
jefersonsm23 <jefersonkillua@gmail.com>
2024-02-17 16:46:03 -0300

    configurando

---
+++ u1_exercicio2/src/main/java/br/com/jeferson/cadpessoas_jeferson/CadpessoasJefersonApplication.java
@@ -0,0 +1,13 @@
+package br.com.jeferson.cadpessoas_jeferson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasJefersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasJefersonApplication.class, args);
+	}
+
+}
---
+++ u1_exercicio2/src/test/java/br/com/jeferson/cadpessoas_jeferson/CadpessoasJefersonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jeferson.cadpessoas_jeferson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasJefersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
